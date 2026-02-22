36aa39379e22143e18ecf3988f8565dd5f2972ae
cnk0909 <alkancanku@gmail.com>
2026-02-22 21:47:43 +0300

    M3L1

--- M2L3/main.py
+++ M2L3/main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/anasayfa')
+def index2():
+    return render_template('index.html')
+
+@app.route('/hava')
+def weather():
+    return render_template('havadurumu.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
